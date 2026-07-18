f3f033f82400984d322461f80a30f4c5606d4b7c
arahel <abhishekrahel@example.com>
2026-07-18 19:29:18 +0530

    tags added

--- Projects/Terraform/pipeline-test/main.tf
+++ Projects/Terraform/pipeline-test/main.tf
@@ -2,4 +2,9 @@ resource "aws_instance" "infra_by_pipeline" {
   ami           = "ami-0b6d9d3d33ba97d99"
   instance_type = var.instance_type
 
+  tags = {
+    Name = "server_by_pipeline"
+
+  }
+
 }
