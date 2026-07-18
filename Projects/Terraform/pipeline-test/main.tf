91d82cc4c578784c76cafb58207c66d786b21e14
arahel <abhishekrahel@example.com>
2026-07-18 19:01:43 +0530

    fmt checked

--- Projects/Terraform/pipeline-test/main.tf
+++ Projects/Terraform/pipeline-test/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "infra_by_pipeline" {
-    ami = "ami-0b6d9d3d33ba97d99"
-    instance_type = var.instance_type
+  ami           = "ami-0b6d9d3d33ba97d99"
+  instance_type = var.instance_type
 
 }
