2acd6fb9853e7ccf2190ef30fc5f3799d3295c2d
arahel <abhishekrahel@example.com>
2026-07-18 22:27:25 +0530

    tfsec scan

--- Projects/Terraform/pipeline-test/main.tf
+++ Projects/Terraform/pipeline-test/main.tf
@@ -2,6 +2,20 @@ resource "aws_instance" "infra_by_pipeline" {
   ami           = "ami-0b6d9d3d33ba97d99"
   instance_type = var.instance_type
 
+  #This forces the use of IMDSv2, which is AWS's more secure metadata service.
+
+  metadata_options {
+  http_endpoint = "enabled"
+  http_tokens   = "required"
+}
+
+root_block_device {
+  encrypted = true
+}
+
+
+
+
   tags = {
     Name = "server_by_pipeline_testing_auto_trigger"
 
