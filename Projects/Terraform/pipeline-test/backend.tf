9840df725cbf0ab935807ff826e8e1b25cf4cace
arahel <abhishekrahel@example.com>
2026-07-19 11:26:27 +0530

    fmt

--- Projects/Terraform/pipeline-test/backend.tf
+++ Projects/Terraform/pipeline-test/backend.tf
@@ -1,11 +1,11 @@
 terraform {
-    backend "s3"{
-        bucket = "arahel-tfstate-2026"
-        key = "arahel-tfstate-2026/terraform.tfstate"
-        encrypt = true
-        region = "us-east-1"
-        dynamodb_table = "terraform-locks"
-    }
+  backend "s3" {
+    bucket         = "arahel-tfstate-2026"
+    key            = "arahel-tfstate-2026/terraform.tfstate"
+    encrypt        = true
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+  }
 }
 
 
