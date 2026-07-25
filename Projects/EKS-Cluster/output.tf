41b3df887a7a689a2b38f008127e42e91c87d049
arahel <abhishekrahel@example.com>
2026-07-25 16:25:23 +0530

    more ouput values

--- Projects/EKS-Cluster/output.tf
+++ Projects/EKS-Cluster/output.tf
@@ -6,3 +6,19 @@ output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
 
 }
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnets" {
+  value = module.vpc.public_subnet_ids
+}
+
+output "private_subnets" {
+  value = module.vpc.private_subnet_ids
+}
+
+output "node_group_name" {
+  value = module.eks.node_group_name
+}
