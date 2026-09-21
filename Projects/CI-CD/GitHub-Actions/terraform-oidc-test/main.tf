fdfa77f8af43cd517d18e44016bbe79cb5354f80
arahel <abhishekrahel@example.com>
2026-09-21 12:55:43 +0530

    test terraform

---
+++ Projects/CI-CD/GitHub-Actions/terraform-oidc-test/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_caller_identity" "current" {}
+
+output "aws_account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "aws_arn" {
+  value = data.aws_caller_identity.current.arn
+}
