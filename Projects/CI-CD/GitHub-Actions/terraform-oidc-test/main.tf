4a7429463ef1bafdb7b2e25f29ce2ad6fe7fb07d
arahel <abhishekrahel@example.com>
2026-09-23 19:33:02 +0530

    added public access block

--- Projects/CI-CD/GitHub-Actions/terraform-oidc-test/main.tf
+++ Projects/CI-CD/GitHub-Actions/terraform-oidc-test/main.tf
@@ -14,16 +14,30 @@ data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "terraform_test" {
   bucket_prefix = "devops-terraform-security-test-"
+
+  
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_test" {
   bucket = aws_s3_bucket.terraform_test.id
+
+  
+
 
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "terraform_test" {
+  bucket = aws_s3_bucket.terraform_test.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 output "aws_account_id" {
