0212c8e42f6a8daf1da89e11d322fc2753c09b2e
arahel <abhishekrahel@example.com>
2026-09-23 21:30:28 +0530

    fomatting done

--- Projects/CI-CD/GitHub-Actions/terraform-oidc-test/main.tf
+++ Projects/CI-CD/GitHub-Actions/terraform-oidc-test/main.tf
@@ -28,7 +28,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_test" {
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm     = "aws:kms"
+      sse_algorithm = "aws:kms"
       # kms_master_key_id = aws_kms_key.terraform_test.arn
     }
   }
