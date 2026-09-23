b1ddb472fa4e20e2086ec01df1d113bba8ffecf2
arahel <abhishekrahel@example.com>
2026-09-23 21:28:02 +0530

    KMS key removed test

--- Projects/CI-CD/GitHub-Actions/terraform-oidc-test/main.tf
+++ Projects/CI-CD/GitHub-Actions/terraform-oidc-test/main.tf
@@ -26,13 +26,10 @@ resource "aws_kms_key" "terraform_test" {
 resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_test" {
   bucket = aws_s3_bucket.terraform_test.id
 
-
-
-
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm     = "aws:kms"
-      kms_master_key_id = aws_kms_key.terraform_test.arn
+      # kms_master_key_id = aws_kms_key.terraform_test.arn
     }
   }
 }
