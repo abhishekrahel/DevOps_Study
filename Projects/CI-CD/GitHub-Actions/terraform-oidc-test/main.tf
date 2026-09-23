fc4e620f65d9d0e21a3464062cc1b6373c7c8ad6
arahel <abhishekrahel@example.com>
2026-09-23 19:04:10 +0530

    s3 bucket creation with encryption

--- Projects/CI-CD/GitHub-Actions/terraform-oidc-test/main.tf
+++ Projects/CI-CD/GitHub-Actions/terraform-oidc-test/main.tf
@@ -12,10 +12,28 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
+resource "aws_s3_bucket" "terraform_test" {
+  bucket_prefix = "devops-terraform-security-test-"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_test" {
+  bucket = aws_s3_bucket.terraform_test.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
 output "aws_account_id" {
   value = data.aws_caller_identity.current.account_id
 }
 
 output "aws_arn" {
   value = data.aws_caller_identity.current.arn
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.terraform_test.id
 }
