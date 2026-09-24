8a679040e27572a2ace3bf5ecda60736cdba83e4
arahel <abhishekrahel@example.com>
2026-09-24 22:11:54 +0530

    deployment on ec2

---
+++ Projects/CI-CD/GitHub-Actions/ec2-deployment/deploy_to_ec2.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
